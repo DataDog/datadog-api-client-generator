0c49d2237ee9e894e6cab173349dced840488521
Thomas Hervé <thomas.herve@datadoghq.com>
2022-07-20 16:59:04 +0200

    Add apis function

--- src/datadog_api_client_generator/openapi.py
+++ src/datadog_api_client_generator/openapi.py
@@ -9,3 +9,14 @@ def load(filename):
     path = pathlib.Path(filename)
     with path.open() as fp:
         return JsonRef.replace_refs(yaml.load(fp, Loader=CSafeLoader))
+
+
+def apis(spec):
+    operations = {}
+
+    for path, endpoints in spec["paths"].items():
+        for method, operation in endpoints.items():
+            tag = operation["tags"][0]
+            operations.setdefault(tag, []).append((path, method, operation))
+
+    return operations
