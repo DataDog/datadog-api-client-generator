d8357271dc107331d3ba92c06219a3a1d8de8e0a
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2024-01-09 13:10:29 -0500

    introduce base openapi class and generator

---
+++ src/datadog_api_client_generator/openapi/schema_model.py
@@ -0,0 +1,140 @@
+from __future__ import annotations
+from typing import Any, Dict, List, Literal, Optional, Union, TypeAlias
+
+from datadog_api_client_generator.openapi.shared_model import _Base, RefObject
+from datadog_api_client_generator.openapi.utils import Empty, OptionalEmpty, StrBool
+
+
+class Schema(_Base):
+    name: OptionalEmpty[str] = Empty()
+    description: OptionalEmpty[str] = Empty()
+    required: Optional[List[str]] = list()
+    type: OptionalEmpty[Literal["string", "number", "integer", "boolean", "array", "object"]] = Empty()
+    format: OptionalEmpty[
+        Literal["int32", "int64", "float", "double", "byte", "binary", "date", "date-time", "password", "email", "uuid"]
+    ] = Empty()
+    deprecated: OptionalEmpty[StrBool] = Empty()
+    example: OptionalEmpty[Any] = Empty()
+    default: OptionalEmpty[Any] = Empty()
+    nullable: OptionalEmpty[StrBool] = Empty()
+    additionalProperties: OptionalEmpty[Union[bool, SchemaType]] = Empty()
+    maxLength: OptionalEmpty[int] = Empty()
+    minLength: OptionalEmpty[int] = Empty()
+    maximum: OptionalEmpty[int] = Empty()
+    minimum: OptionalEmpty[int] = Empty()
+    pattern: OptionalEmpty[str] = Empty()
+    maxItems: OptionalEmpty[int] = Empty()
+    minItems: OptionalEmpty[int] = Empty()
+    exclusiveMinimum: OptionalEmpty[StrBool] = Empty()
+    exclusiveMaximum: OptionalEmpty[StrBool] = Empty()
+    readOnly: OptionalEmpty[StrBool] = Empty()
+    writeOnly: OptionalEmpty[StrBool] = Empty()
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        if isinstance(self.additionalProperties, SchemaType):
+            mapping.update(self.additionalProperties.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+class OneOfSchema(Schema):
+    oneOf: List[SchemaType]
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        for oneOf in self.oneOf:
+            mapping.update(oneOf.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+class EnumSchema(Schema):
+    enum: List[Union[str, int, float]]
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        return mapping
+
+
+class AllOfSchema(Schema):
+    allOf: List[SchemaType]
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        for allOf in self.allOf:
+            mapping.update(allOf.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+class AnyOfSchema(Schema):
+    anyOf: List[SchemaType]
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        for anyOf in self.anyOf:
+            mapping.update(anyOf.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+class ArraySchema(Schema):
+    items: SchemaType
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        mapping.update(self.items.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+class ObjectSchema(Schema):
+    properties: Dict[str, SchemaType]
+
+    def schemas_by_name(self, mapping: Optional[Dict[str, SchemaType]] = None) -> Dict[str, SchemaType]:
+        if mapping is None:
+            mapping = {}
+
+        if self.name:
+            mapping[self.name] = self
+
+        for k, p in self.properties.items():
+            mapping.update(p.schemas_by_name(mapping=mapping))
+
+        return mapping
+
+
+SchemaType: TypeAlias = Union[
+    RefObject, ArraySchema, AnyOfSchema, AllOfSchema, EnumSchema, OneOfSchema, ObjectSchema, Schema
+]
---
+++ src/datadog_api_client_generator/openapi/utils.py
@@ -0,0 +1,51 @@
+from jinja2 import Undefined
+import yaml
+from pathlib import PosixPath
+from typing import Annotated, Any, Dict, Optional, Self, Tuple, TypeVar, Union
+
+from pydantic import GetCoreSchemaHandler, PlainValidator
+from pydantic_core import core_schema
+from jsonref import JsonRef
+
+
+HEADER_ANY_TYPE = "*/*"
+
+
+# Annotated str class. Used to convert string to Bool values.
+StrBool = Annotated[str, PlainValidator(lambda v: {"true": True, "false": False}[str(v).lower()])]
+
+
+# Helper class that will always evaluate to falsey.
+# We use this in schemas to help defferentiate between explicit null and omitted.
+class Empty(Undefined):
+    def __bool__(self) -> bool:
+        return False
+
+    def __copy__(self):
+        ...
+
+    def __deepcopy__(self, memo: Any):
+        ...
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type: Any, handler: GetCoreSchemaHandler) -> any:
+        return core_schema.any_schema()
+
+
+T = TypeVar("T")
+OptionalEmpty = Union[T, type(None), Empty]
+
+
+def load_yaml(path: PosixPath) -> Dict:
+    """
+    Return openapi specification from yaml file.
+    """
+    with path.open() as fp:
+        return yaml.load(fp, Loader=yaml.CSafeLoader)
+
+
+def get_name_and_path_from_ref(ref: str) -> Tuple[str, str]:
+    """
+    Return name and path from $ref value.
+    """
+    return ref.split("/")[-2:]
