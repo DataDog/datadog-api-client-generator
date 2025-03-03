678f4397c79a3b5ed88ddd0ff471960aa37118bb
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2025-03-03 16:18:34 -0500

    add ruff support and licence checks

--- src/datadog_api_client_generator/openapi/operation_model.py
+++ src/datadog_api_client_generator/openapi/operation_model.py
@@ -1,3 +1,6 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
+#
+# This product includes software developed at Datadog (https://www.datadoghq.com/  Copyright 2025 Datadog, Inc.
 from __future__ import annotations
 
 from typing import Any, Dict, Iterator, List, Optional, TypeAlias, Union
